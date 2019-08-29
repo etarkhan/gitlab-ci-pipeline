66ceaf6efd253a27cc480939278d83507ad4fba4
a.tarkhani <a.tarkhani@sportradar.com>
2019-08-29 09:09:29 +0200

    initial commit

---
+++ php/scripts/alpine/packages.sh
@@ -0,0 +1,32 @@
+#!/usr/bin/env bash
+
+set -euf -o pipefail
+
+apk update \
+&& apk --no-cache upgrade \
+&& apk --no-cache add \
+    g++ \
+    gcc \
+    git \
+    grep \
+    libc-dev \
+    make \
+    mariadb-client \
+    openssh-client \
+    openssl \
+    python \
+    rsync \
+    sudo \
+    zip
+
+# persistent / runtime deps
+apk add --no-cache --virtual .persistent-deps \
+		ca-certificates \
+		tar \
+		xz \
+    curl
+apk add --no-cache \
+    autoconf \
+    build-base \
+    file \
+    openssl-dev
